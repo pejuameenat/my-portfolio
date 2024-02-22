d4ce7f1ce6815bedbf83aace99f7a78b83cad2b9
pejuameenat <104262132+pejuameenat@users.noreply.github.com>
2024-02-23 00:26:56 +0100

    mode settings

--- src/components/App.js
+++ src/components/App.js
@@ -6,10 +6,10 @@ import Projects from './Projects';
 import Contact from './Contact';
 import Footer from './Footer';
 function App(props){
-    const [mode, setMode] = useState(false)
+    const [mode, setMode] = useState(props.mode)
     let styles = {
-      backgroundColor: props.mode ? '#000' : '#fff',
-      color: props.mode ? '#fff' : '#000',
+      backgroundColor: mode ? '#000' : '#fff',
+      color: mode ? '#fff' : '#000',
     }
     function modeSetter(){
         setMode((prevMode)=> !prevMode)
